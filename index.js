016096bf55a97aac29083ad52a0551d1e32b90c0
samitkumarsinha <samitkumarsinha@gmail.com>
2021-05-31 11:02:55 +0530

    add files

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+const profiles = 
+[
+    {
+        'name': 'ram',
+        'roll' : 14
+    },
+    {
+        'name': 'shyam',
+        'roll' : 15
+    }
+];
+app.get('/', (req, res) => {
+    res.json(profiles)
+})
+var server = app.listen(port, () => {
+    console.log(`Server running at port ${port}`)
+})
