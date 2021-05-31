031aa7bd259cb17225da3f64a85d367e44fa12d5
samitkumarsinha <samitkumarsinha@gmail.com>
2021-05-31 14:44:45 +0530

    add files

--- index.js
+++ index.js
@@ -3,19 +3,26 @@ var app = express();
 var port = process.env.PORT || 3000;
 var cors = require('cors')
 app.use(cors());
-const profiles = 
-[
-    {
-        'name': 'ram',
-        'roll' : 14
-    },
-    {
-        'name': 'shyam',
-        'roll' : 15
-    }
-];
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const mongoose = require("mongoose");
+var uri = 'mongodb+srv://samdb:unicorn1@cluster0.idow2.mongodb.net/school';
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+var userschema = new mongoose.Schema({
+   username: String,
+   password: String,
+   email: String,
+   role: String
+}
+);
+mongoose.connection.on('connected', (err, res) => {
+   console.log('connected successfully!!!');
+})
 app.get('/', (req, res) => {
-    res.json(profiles)
+    var users = mongoose.model('users', userschema);
+    users.find({}, function(err, data){
+        res.json(data)
+    });    
 })
 var server = app.listen(port, () => {
     console.log(`Server running at port ${port}`)
