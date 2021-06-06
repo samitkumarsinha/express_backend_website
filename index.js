d8822eb007d6a4a6190472a50356dba1332d6402
samitkumarsinha <samitkumarsinha@gmail.com>
2021-06-06 10:11:27 +0530

    init

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var port = process.env.PORT || 3000;
 var app = express();
 app.get('/', (req, res) => {
-    res.send(`Running at port ${port}`)
+    res.send(`Running at port ${port}..`)
 })
 app.listen(port, () => {
     console.log(`Server running at port ${port}`);
