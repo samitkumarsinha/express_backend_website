e74d94ee43c1c8928f45ea03c20111ecfbd23db3
samitkumarsinha <samitkumarsinha@gmail.com>
2021-06-07 07:59:38 +0530

    cors

--- Controllers/mailController.js
+++ Controllers/mailController.js
@@ -1,6 +1,29 @@
 module.exports = {
     sendMail(req, res){
-        console.log(req.body)
-        res.send(req.body)
+        var nodemailer = require('nodemailer');
+        var sender = req.body;
+        var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'samitkumarsinhaworks@gmail.com',
+            pass: 'vtwggnphijxhvhqc'
+        }
+        });
+
+        var mailOptions = {
+            from: sender.sendername + ' <' + sender.emailid + '>',
+            to: 'samitkumarsinhaworks@gmail.com',
+            subject: 'Mail from website by ' + sender.sendername,
+            text: sender.mailbody
+        };
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+                console.log(mailOptions)
+                res.status(200).json({"status":"sent"});
+            }
+        });
     }
 }
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var api = require('./Router/api');
 var express = require('express')
 var port = process.env.PORT || 3000;
 var app = express();
+var cors = require('cors');
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api)
